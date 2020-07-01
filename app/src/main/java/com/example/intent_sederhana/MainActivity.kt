c4c70f662cda94be42f5dd25270148741ebc1585
ardidiskominfokampar <ardidiskominfokampar@gmail.com>
2020-07-01 23:49:44 +0700

    Update

--- app/src/main/java/com/example/intent_sederhana/MainActivity.kt
+++ app/src/main/java/com/example/intent_sederhana/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.intent_sederhana
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnMoveActivity : Button = findViewById(R.id.btn_move_activity)
+        val btnMoveActivityWithData : Button = findViewById(R.id.btn_move_activity_with_data)
+        btnMoveActivity.setOnClickListener(this)
+        btnMoveActivityWithData.setOnClickListener(this)
+
     }
+
+    override fun onClick(v: View) {
+        when (v.id){
+            R.id.btn_move_activity -> {
+                 val moveIntent = Intent(this@MainActivity, MoveActivity::class.java )
+                startActivity(moveIntent)
+            }
+            R.id.btn_move_activity_with_data -> {
+                val moveIntentWithData = Intent(this@MainActivity, MoveWithData::class.java )
+                moveIntentWithData.putExtra(MoveWithData.EXTRA_NAME,"Ardiansyah")
+                moveIntentWithData.putExtra(MoveWithData.EXTRA_AGE,28)
+                startActivity(moveIntentWithData)
+            }
+
+        }
+    }
+
 }
+
